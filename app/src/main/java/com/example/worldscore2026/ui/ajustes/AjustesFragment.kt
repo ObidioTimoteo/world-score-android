7a2a98c73fbf983d67ed2574e2517e0729876230
ObidioTimoteo <kxsgch@hotmail.com>
2026-03-11 21:11:44 +0100

    Duodécimo commit - Primera versión del Fragment de Opciones. Primera versión de colors, themes, styles para Default, Canadá, USA, México

---
+++ app/src/main/java/com/example/worldscore2026/ui/ajustes/AjustesFragment.kt
@@ -0,0 +1,37 @@
+package com.example.worldscore2026.ui.ajustes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.worldscore2026.R
+
+class AjustesFragment : Fragment() {
+
+    private lateinit var switchModoOscuro: Switch
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        switchModoOscuro = view.findViewById(R.id.switchModoOscuro)
+
+        switchModoOscuro.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/worldscore2026/ui/partidos/JornadaFragment.kt
+++ app/src/main/java/com/example/worldscore2026/ui/partidos/JornadaFragment.kt
@@ -1,7 +1,6 @@
 package com.example.worldscore2026.ui.partidos
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
